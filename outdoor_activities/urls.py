f4a948e39d457ffcbea1dcfdac72185eff27c7e8
Walid <walid.r.rashed@gmail.com>
2022-01-10 00:24:21 +0300

    chore: gitignore file updated

--- outdoor_activities/urls.py
+++ outdoor_activities/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('Home Page')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('',home)
+
 ]
